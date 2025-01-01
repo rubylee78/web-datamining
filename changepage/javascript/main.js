ae6ab1ab1c0d636ba24dbfec281b9faea586f039
rubylee78 <20010708ruby@gmail.com>
2025-01-02 01:28:07 +0800

    Add files via upload

---
+++ changepage/javascript/main.js
@@ -0,0 +1,17 @@
+function goPage1(){
+
+	window.location="page1.html";
+	alert("切換到第一頁")
+}
+
+function goPage2(){
+
+	window.location="page2.html";
+	alert("切換到第二頁")
+}
+
+function goPage3(){
+
+	window.location="page3.html";
+	alert("切換到第三頁")
+}
